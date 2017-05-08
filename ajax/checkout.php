6a55b48bb63902f94115b6c365db0138d1e25551
Dayan Betancourt <dkbetancourt@gmail.com>
2017-05-07 20:18:58 -0400

    Email de la Orden

--- ajax/checkout.php
+++ ajax/checkout.php
@@ -4,13 +4,21 @@ require("../configuracion/inicio.php");
 // Clases
 require("../clases/clase_orden.php");
 require("../clases/clase_orden_presentacion.php");
+require("../clases/clase_producto.php");
+require("../clases/clase_opcion1.php");
+require("../clases/clase_opcion2.php");
 require("../clases/clase_general.php");
+require("../clases/clase_email.php");
 require_once("../stripe-php-3.23.0/init.php");
 
 // Objetos
 $orden = new Orden($conexion);
 $orden_presentacion = new OrdenPresentacion($conexion);
+$producto = new Producto($conexion);
+$opcion1 = new Opcion1($conexion);
+$opcion2 = new Opcion2($conexion);
 $general = new General($conexion);
+$eemail = new Email();
 
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
@@ -40,9 +48,64 @@ $pedido = $_SESSION['orden']['pedido'];
 if($orden->insertar($nombre, $email, $telefono, $direccion, $zipcode, $ciudad, $instrucciones, $id_hotel, $hotel_nombre, $habitacion, $delivery, $tax, $propina, $total)) {
 	$id_orden = $general->ultimo_id($conexion);
 
+	$html_carrito = '';
+	$html_carrito .= '<table width="100%" style="color: #666666; font-family: Arial; font-size: 12px; line-height: 18px;">';
+	$html_carrito .= '<thead>';
+	$html_carrito .= '<tr>';
+	$html_carrito .= '<th style="background-color: #F5F5F5;">Item</th>';
+	$html_carrito .= '<th style="background-color: #F5F5F5;">Option 1</th>';
+	$html_carrito .= '<th style="background-color: #F5F5F5;">Option 2</th>';
+	$html_carrito .= '<th style="background-color: #F5F5F5; text-align: right;">Qty</th>';
+	$html_carrito .= '<th style="background-color: #F5F5F5; text-align: right;">Price</th>';
+	$html_carrito .= '<th style="background-color: #F5F5F5; text-align: right;">Subtotal</th>';
+	$html_carrito .= '</tr>';
+	$html_carrito .= '</thead>';
+	$html_carrito .= '<tbody>';
+
+	$subtotal1 = 0;
+	$subtotal2 = 0;
 	foreach($pedido as $item) {
 		$orden_presentacion->insertar($id_orden, $item['producto'], $item['opcion1'], $item['opcion2'], $item['precio'], $item['cantidad'], $item['instrucciones']);
+
+		if($producto->datos($item['producto'])) { $producto_nombre = $producto->nombre; } else { $producto_nombre = '---'; }
+		if($opcion1->datos($item['opcion1'])) { $opcion1_nombre = $opcion1->nombre; } else { $opcion1_nombre = '---'; }
+		if($opcion2->datos($item['opcion2'])) { $opcion2_nombre = $opcion2->nombre; } else { $opcion2_nombre = '---'; }
+
+		$subtotal1 = $item['cantidad'] * $item['precio'];
+		$subtotal2 += $subtotal1;
+
+		$html_carrito .= '<tr>';
+		$html_carrito .= '<td>'.$producto_nombre.'</td>';
+		$html_carrito .= '<td>'.$opcion1_nombre.'</td>';
+		$html_carrito .= '<td>'.$opcion2_nombre.'</td>';
+		$html_carrito .= '<td style="text-align: right;">'.$item['cantidad'].'</td>';
+		$html_carrito .= '<td style="text-align: right;">$'.$item['precio'].'</td>';
+		$html_carrito .= '<td style="text-align: right;">$'.number_format($subtotal1, 2).'</td>';
+		$html_carrito .= '</tr>';
 	}
+
+	$html_carrito .= '<tr>';
+	$html_carrito .= '<td colspan="5" style="background-color: #F5F5F5; text-align: right;"><strong>Subtotal</strong></td>';
+	$html_carrito .= '<td style="background-color: #F5F5F5; text-align: right;">'.number_format($subtotal2, 2).'</td>';
+	$html_carrito .= '</tr>';
+	$html_carrito .= '<tr>';
+	$html_carrito .= '<td colspan="5" style="background-color: #F5F5F5; text-align: right;"><strong>Delivery Fee</strong></td>';
+	$html_carrito .= '<td style="background-color: #F5F5F5; text-align: right;">'.number_format($delivery, 2).'</td>';
+	$html_carrito .= '</tr>';
+	$html_carrito .= '<tr>';
+	$html_carrito .= '<td colspan="5" style="background-color: #F5F5F5; text-align: right;"><strong>Tax</strong></td>';
+	$html_carrito .= '<td style="background-color: #F5F5F5; text-align: right;">'.number_format($tax, 2).'</td>';
+	$html_carrito .= '</tr>';
+	$html_carrito .= '<tr>';
+	$html_carrito .= '<td colspan="5" style="background-color: #F5F5F5; text-align: right;"><strong>Tip</strong></td>';
+	$html_carrito .= '<td style="background-color: #F5F5F5; text-align: right;">'.number_format($propina, 2).'</td>';
+	$html_carrito .= '</tr>';
+	$html_carrito .= '<tr>';
+	$html_carrito .= '<td colspan="5" style="background-color: #F5F5F5; text-align: right;"><strong>Total</strong></td>';
+	$html_carrito .= '<td style="background-color: #F5F5F5; text-align: right;">'.number_format($total, 2).'</td>';
+	$html_carrito .= '</tr>';
+	$html_carrito .= '</tbody>';
+	$html_carrito .= '<table>';
 
 	session_unset();                 // Vacia las variables de sesion
 	session_destroy();               // Destruye la sesion
@@ -67,6 +130,9 @@ if($orden->insertar($nombre, $email, $telefono, $direccion, $zipcode, $ciudad, $
     // The card has been declined
     $orden->actualizar_estado($id_orden, 3);
   }
+
+  // Enviar email
+  $eemail->enviar_orden($id_orden, $nombre, $email, $telefono, $direccion, $zipcode, $ciudad, $id_hotel, $hotel_nombre, $habitacion, $instrucciones, $html_carrito);
 
 	$url = '?msjError=E';
 } else {
--- clases/clase_email.php
+++ clases/clase_email.php
@@ -3,7 +3,7 @@ class Email {
   public function enviar_contrasena($correo, $contrasena, $nombre, $apellido) {
     $arrhtml_mail = file($GLOBALS['app_root'].'/plantillas/email_contrasena.html');
 		
-	$html_mail = '';
+  	$html_mail = '';
     for($i = 0; $i < count($arrhtml_mail); $i++) {
       $html_mail .= $arrhtml_mail[$i];
     }
@@ -39,7 +39,7 @@ class Email {
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=utf-8\r\n";
     $headers .= "From: Delivery Troop <hello@deliverytroop.com>\r\n";
-    mail('hello@deliverytroop.com', $asunto, $contenido, $headers); #ENVIO DEL EMAIL CON LOS DATOS#
+    mail('deliverytroop@gmail.com', $asunto, $contenido, $headers); #ENVIO DEL EMAIL CON LOS DATOS#
   }
 
   public function enviar_respuesta_comentario($email, $usuario, $respuesta) {
@@ -62,7 +62,7 @@ class Email {
     mail($email, $asunto, $contenido, $headers); #ENVIO DEL EMAIL CON LOS DATOS#
   }
   
-  public function enviar_orden($id_orden, $nombre, $email, $telefono, $direccion, $zipcode, $ciudad, $instrucciones, $html_carrito) {
+  public function enviar_orden($id_orden, $nombre, $email, $telefono, $direccion, $zipcode, $ciudad, $hotel_id, $hotel_nombre, $hotel_habitacion, $instrucciones, $html_carrito) {
     $arrhtml_mail = file($GLOBALS['app_root'].'/plantillas/email_orden.html');
         
     $html_mail = '';
@@ -70,12 +70,23 @@ class Email {
       $html_mail .= $arrhtml_mail[$i];
     }
     
+    $html_mail = str_replace("#ORDEN_ID#",$id_orden,$html_mail);
     $html_mail = str_replace("#NOMBRE#",$nombre,$html_mail);
     $html_mail = str_replace("#EMAIL#",$email,$html_mail);
     $html_mail = str_replace("#TELEFONO#",$telefono,$html_mail);
     $html_mail = str_replace("#DIRECCION#",$direccion,$html_mail);
     $html_mail = str_replace("#ZIPCODE#",$zipcode,$html_mail);
     $html_mail = str_replace("#CIUDAD#",$ciudad,$html_mail);
+
+    $hotel = '';
+    if($hotel_id > 0) {
+      $hotel .= '<tr>';
+      $hotel .= '<td style="padding: 4px; width: 50%; border-bottom: 1px solid #FFBE00;">Hotel: <strong>'.$hotel_nombre.'</strong></td>';
+      $hotel .= '<td style="padding: 4px; width: 50%; border-bottom: 1px solid #FFBE00;">Room: <strong>'.$hotel_habitacion.'</strong></td>';
+      $hotel .= '</tr>';
+    }
+
+    $html_mail = str_replace("#HOTEL#",$hotel,$html_mail);
     $html_mail = str_replace("#INSTRUCCIONES#",$instrucciones,$html_mail);
     $html_mail = str_replace("#HTML_CARRITO#",$html_carrito,$html_mail);
     
